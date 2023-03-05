3250f8ee3e45a5f4813d7bef1027e38e1494c957
DianZhao <55634814+DianZzzz@users.noreply.github.com>
2023-03-04 22:43:30 -0600

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,34 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+const config = require("./src/config");
+
 module.exports = {
   siteMetadata: {
-    title: `My Portfolio Website`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: config.siteTitle,
+    siteUrl: config.siteUrl,
+    description: config.siteDescription,
+    image: "/og.png",
   },
-  plugins: [],
-}
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Dian Zhao",
+        short_name: "Dian Zhao",
+        start_url: "/",
+        background_color: config.colors.darkNavy,
+        theme_color: config.colors.navy,
+        display: "minimal-ui",
+        icon: "src/images/logo.png",
+      },
+    },
+  ],
+};
---
+++ src/config.js
@@ -0,0 +1,68 @@
+module.exports = {
+  email: "aura-zhao@hotmail.com",
+  siteTitle: "Dian Zhao | Data Scientist | Equity Analyst | Consultant",
+  siteDescription: "Dian Zhao is a data scientist at BCG.",
+  siteKeywords: "dian zhao, dian, dianzzzz, data scientist, consultant, ",
+  siteUrl: "",
+  siteLanguage: "en_US",
+
+  socialMedia: [
+    {
+      name: "GitHub",
+      url: "https://github.com/dianzzzz",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/d.zhao",
+    },
+    {
+      name: "Twitter",
+      url: "https://twitter.com/sassypandacap",
+    },
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/dian-zhao",
+    },
+  ],
+
+  navLinks: [
+    {
+      name: "About",
+      url: "/#about",
+    },
+    {
+      name: "Experience",
+      url: "/#jobs",
+    },
+    {
+      name: "Work",
+      url: "/#projects",
+    },
+    {
+      name: "Contact",
+      url: "/#contact",
+    },
+  ],
+
+  colors: {
+    green: "#64ffda",
+    navy: "#0a192f",
+    darkNavy: "#020c1b",
+  },
+
+  srConfig: (delay = 200, viewFactor = 0.25) => ({
+    origin: "bottom",
+    distance: "20px",
+    duration: 500,
+    delay,
+    rotate: { x: 0, y: 0, z: 0 },
+    opacity: 0,
+    scale: 1,
+    easing: "cubic-bezier(0.645, 0.045, 0.355, 1)",
+    mobile: true,
+    reset: false,
+    useDelay: "always",
+    viewFactor,
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  }),
+};
