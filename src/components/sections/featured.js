6dcf4245fc54470d930b75788a5ea0e099b720b4
DianZhao <55634814+DianZzzz@users.noreply.github.com>
2023-03-06 11:09:20 -0600

    Update

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -264,7 +264,7 @@ const Featured = () => {
     {
       featured: allMarkdownRemark(
         filter: { fileAbsolutePath: { regex: "/content/featured/" } }
-        sort: { fields: [frontmatter___date], order: ASC }
+        sort: { frontmatter: { date: ASC } }
       ) {
         edges {
           node {
@@ -282,7 +282,6 @@ const Featured = () => {
               tech
               github
               external
-              cta
             }
             html
           }
